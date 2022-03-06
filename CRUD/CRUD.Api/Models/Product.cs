a04a24fe769a3b13a5e3786b3c54a5df0122159d
dudushy <eduardotalarico1337@gmail.com>
2022-03-06 16:28:27 -0300

    no progress at all

---
+++ CRUD/CRUD.Api/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Weight { get; set; }
+    }
+}
---
+++ CRUD/CRUD.Api/Models/Provider.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD.Api.Models
+{
+    public class Provider
+    {
+        public string Cnpj { get; set; }
+        public string Name { get; set; }
+        public string RegisterDate { get; set; }
+    }
+}
