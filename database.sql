fb0b98267bdb529ee82073395e5788c92a50018b
dudushy <eduardotalarico1337@gmail.com>
2022-03-06 15:06:40 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,25 +1,25 @@
-CREATE DATABASE 'CRUD';
+CREATE DATABASE [dbo.CRUD];
 
 CREATE TABLE tb_users(
-    u_cpf           VARCHAR(11) NOT NULL,
-    u_name          VARCHAR(60) NOT NULL,
+    u_cpf           NVARCHAR(11) NOT NULL,
+    u_name          NVARCHAR(60) NOT NULL,
     u_birth_date    DATE NOT NULL,
-    u_email         VARCHAR(60) NOT NULL,
-    u_password      VARCHAR(30) NOT NULL,
+    u_email         NVARCHAR(60) NOT NULL,
+    u_password      NVARCHAR(30) NOT NULL,
     PRIMARY KEY(u_cpf)
 );
 
 CREATE TABLE tb_products(
-    pd_id            INTEGER NOT NULL,
-    pd_name          VARCHAR(60) NOT NULL,
-    pd_desc          VARCHAR(120) NOT NULL,
+    pd_id            INT IDENTITY(1, 1),
+    pd_name          NVARCHAR(60) NOT NULL,
+    pd_desc          NVARCHAR(120) NOT NULL,
     pd_weight        FLOAT NOT NULL,
     PRIMARY KEY(pd_id)
 );
 
 CREATE TABLE tb_providers(
-    pv_cnpj          VARCHAR(14) NOT NULL,
-    pv_name          VARCHAR(60) NOT NULL,
+    pv_cnpj          NVARCHAR(14) NOT NULL,
+    pv_name          NVARCHAR(60) NOT NULL,
     pv_reg_date      DATE NOT NULL,
     PRIMARY KEY(pv_cnpj)
 );
